877d1824bda69bfaea9b5a7e8d74fa44b0767d9b
anheiqishi25 <33386509+anheiqishi25@users.noreply.github.com>
2020-11-05 13:35:43 +0800

    Add 3gpp_sync server file
    Visit by type "https://anheiqishi25.github.io/a"

---
+++ a/cgi-bin/test.py
@@ -0,0 +1,11 @@
+
+import os
+
+print ("Content-type: text/html")
+print ()
+print ("<meta charset=\"utf-8\">")
+print ("<b>Env Var</b><br>")
+print ("<ul>")
+for key in os.environ.keys():
+    print ("<li><span style='color:green'>%30s </span> : %s </li>" % (key,os.environ[key]))
+print ("</ul>")
---
+++ a/http.serve_with_cgi.bat
@@ -0,0 +1,3 @@
+@echo off
+echo "http://localhost:8000/
+python -m http.server --cgi 8000
