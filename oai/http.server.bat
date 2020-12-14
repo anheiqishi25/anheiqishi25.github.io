e51799b809f5d87bcbed9600e6d2885e5792f254
anheiqishi25 <33386509+anheiqishi25@users.noreply.github.com>
2020-12-14 09:10:14 +0800

    Update http.server.bat

--- oai/http.server.bat
+++ oai/http.server.bat
@@ -4,4 +4,4 @@ echo "http://localhost:8000/
 echo %url%
 start http://localhost:8000
 ::python -m http.server --cgi 127.0.0.1 8000
-python -m http.server --bind 127.0.0.1 8000 --directory ./html/
+python -m http.server --bind 127.0.0.1 8000
