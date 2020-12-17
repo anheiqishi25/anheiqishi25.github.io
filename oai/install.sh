77ab9b6b07ede4c01dd5ce0c6db64574adbc475a
anheiqishi25 <33386509+anheiqishi25@users.noreply.github.com>
2020-12-17 17:35:41 +0800

    Create install.sh

---
+++ oai/install.sh
@@ -0,0 +1,22 @@
+sudo apt update
+sudo apt install git cmake
+# Download OAI sources
+git clone https://github.com/OPENAIRINTERFACE/openair-epc-fed.git
+cd openair-epc-fed
+cwd=`pwd`
+./scripts/syncComponents.sh
+
+# Build OAI sources
+git config --global https.proxy https://39.97.175.223:52578
+
+# git config --global --unset https.proxy
+
+
+# Configurations of MME && SPGW-C SPGW-U
+wget https://anheiqishi25.github.io/oai/config.sh
+chmod a+x config.sh
+./config.sh
+# git clone https://github.com/OPENAIRINTERFACE/openair-mme.git
+# git clone https://github.com/OPENAIRINTERFACE/openair-hss.git
+# git clone https://github.com/OPENAIRINTERFACE/openair-spgwc.git
+# git clone https://github.com/OPENAIRINTERFACE/openair-spgwu-tiny.git
