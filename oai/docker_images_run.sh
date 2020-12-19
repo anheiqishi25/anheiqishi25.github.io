fc35481d2dc6ba14caf4689f8c60c194180e7938
anheiqishi25 <33386509+anheiqishi25@users.noreply.github.com>
2020-12-19 17:31:58 +0800

    Update docker_images_run.sh

--- oai/docker_images_run.sh
+++ oai/docker_images_run.sh
@@ -17,9 +17,9 @@ docker start prod-oai-spgwc
 docker start prod-oai-spgwu-tiny
 
 sleep 30
-docker network connect prod-oai-public-net prod-oai-hss
+# docker network connect prod-oai-public-net prod-oai-hss
 # On your EPC Docker Host: recover the MME IP address:
-docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" prod-oai-mme
+# docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" prod-oai-mme
 # Return MME IP address: 192.168.61.3
 # SPGW-U IP address 192.168.61.5
 
@@ -64,7 +64,14 @@ docker exec -it prod-oai-spgwu-tiny /bin/bash -c "cd /openair-spgwu-tiny && chmo
 
 
 echo "################################################################################"
-echo "# run HSS app"
+echo "# log setting"
+echo "################################################################################"
+docker exec -d prod-oai-hss /bin/bash -c "nohup tshark -i eth0 -i eth1 -w /tmp/hss_check_run.pcap 2>&1 > /dev/null"
+docker exec -d prod-oai-mme /bin/bash -c "nohup tshark -i eth0 -i lo:s10 -w /tmp/mme_check_run.pcap 2>&1 > /dev/null"
+docker exec -d prod-oai-spgwc /bin/bash -c "nohup tshark -i eth0 -i lo:p5c -i lo:s5c -w /tmp/spgwc_check_run.pcap 2>&1 > /dev/null"
+docker exec -d prod-oai-spgwu-tiny /bin/bash -c "nohup tshark -i eth0 -w /tmp/spgwu_check_run.pcap 2>&1 > /dev/null"
+echo "################################################################################"
+echo "# run app"
 echo "################################################################################"
 docker exec -d prod-oai-hss /bin/bash -c "nohup ./bin/oai_hss -j ./etc/hss_rel14.json --reloadkey true > hss_check_run.log 2>&1"
 sleep 2
